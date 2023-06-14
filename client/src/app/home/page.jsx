2af1fc29da1b507ec35baef7d7984e3f696ded6b
Juan Mauricio Muñoz <maurikamon@hotmail.com>
2023-06-14 16:43:33 -0300

    home updated

--- client/src/app/home/page.jsx
+++ client/src/app/home/page.jsx
@@ -1,49 +1,76 @@
 import Image from "next/image";
 
+
 export default function Home() {
   return (
-    <div className="w-9/12 m-auto">
-      <div className="flex">
-        <div className="w-1/2 p-8">
-          <Image src="/img/delfin.jpg" width={1000} height={1000} />
+    <div className="container m-auto">
+      <div className="grid grid-cols-1 lg:grid-cols-3 md:grid-cols-1 gap-3">
+        <div className="col-span-1">
+          {/* Contenido de la primera columna */}
+          <div
+            className="h-64 bg-cover bg-center"
+            style={{ backgroundImage: "url(/img/delfin.jpg)" }}
+          >
+            <h1 className="text-white text-2xl p-4">
+              ONG dedicada a la protección y conservación del mundo marino.
+            </h1>
+          </div>
         </div>
-        <div className="w-1/2 mx-auto p-8">
-          <h1 className="text-2xl text-left mt-24 block">
-            Somos una ONG dedicada a la protección y conservación del mundo
-            marino. Trabajamos incansablemente para preservar la vida marina y
-            los ecosistemas costeros, promoviendo la sostenibilidad y educando
-            sobre la importancia de nuestros océanos.
-          </h1>
+        <div className="col-span-1">
+          {/* Contenido de la segunda columna */}
+          <div
+            className="h-64 bg-cover bg-center"
+            style={{ backgroundImage: "url(/img/foca.jpg)" }}
+          >
+            <h1 className="text-white text-2xl p-4">
+              Nuestra ONG se dedica a la investigación y monitoreo de especies marinas.
+            </h1>
+          </div>
         </div>
-      </div>
-      <div className="flex">
-        <div className="w-1/2 p-5">
-          <h1 className="text-2xl text-left mt-24 block">
-            Nuestra ONG se dedica a la investigación y monitoreo de especies
-            marinas, colaborando con científicos y expertos para impulsar la
-            conservación de tortugas marinas, cetáceos y arrecifes coralinos.
-            Además, implementamos programas de limpieza de playas y promovemos
-            el turismo responsable.
-          </h1>
-        </div>
-        <div className="w-1/2">
-          <Image src="/img/foca.jpg" width={1000} height={1000} />
-        </div>
-      </div>
-      <div className="flex">
-        <div className="w-1/2 p-8">
-          <Image src="/img/medusa.jpg" width={1000} height={1000} />
-        </div>
-        <div className="w-1/2 mx-auto p-8">
-          <h1 className="text-2xl text-left mt-24 block">
-            A través de campañas de concienciación y programas educativos
-            nuestra ONG busca inspirar a las comunidades locales y globales a
-            tomar acción por el mundo marino. Trabajamos en colaboración con
-            gobiernos organizaciones internacionales y voluntarios comprometidos
-            para lograr un futuro sostenible para nuestros océanos.
-          </h1>
+        <div className="col-span-1">
+          {/* Contenido de la tercera columna */}
+          <div
+            className="h-64 bg-cover bg-center"
+            style={{ backgroundImage: "url(/img/tiburon.jpg)" }}
+          >
+            <h1 className="text-white text-2xl p-4">
+              Campañas de concienciación y programas educativos.
+            </h1>
+          </div>
         </div>
       </div>
     </div>
   );
-}
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -1,47 +1,47 @@
 import Link from "next/link";
 
 function Navigation() {
-  return (
-    <nav class="bg-gray-400">
-      <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div class="flex items-center justify-between h-16">
-          <div class="flex-shrink-0">
-            <Link href="/" class="text-white text-lg font-bold">
-              Ocean Allies
-            </Link>
-          </div>
-          <div class="hidden md:block">
-            <div class="ml-10 flex items-baseline space-x-4">
-              <Link
-                href="/home"
-                class="text-gray-300 hover:bg-gray-500 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-              >
-                Home
-              </Link>
-              <Link
-                href="/about"
-                class="text-gray-300 hover:bg-gray-500 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-              >
-                About Us
-              </Link>
-              <Link
-                href="/shop"
-                class="text-gray-300 hover:bg-gray-500 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-              >
-                Shop
-              </Link>
-              <Link
-                href="/contact"
-                class="text-gray-300 hover:bg-gray-500 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-              >
-                Contact
-              </Link>
+    return (
+        <nav class="bg-gray-400">
+            <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                <div class="flex items-center justify-between h-16">
+                    <div class="flex-shrink-0">
+                        <Link href="/" class="text-white text-lg font-bold">
+                            Ocean Allies
+                        </Link>
+                    </div>
+                    <div class="hidden md:block">
+                        <div class="ml-10 flex items-baseline space-x-4">
+                            <Link
+                                href="/home"
+                                class="text-gray-300 hover:bg-gray-500 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                            >
+                                Home
+                            </Link>
+                            <Link
+                                href="/about"
+                                class="text-gray-300 hover:bg-gray-500 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                            >
+                                About Us
+                            </Link>
+                            <Link
+                                href="/shop"
+                                class="text-gray-300 hover:bg-gray-500 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                            >
+                                Shop
+                            </Link>
+                            <Link
+                                href="/contact"
+                                class="text-gray-300 hover:bg-gray-500 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                            >
+                                Contact
+                            </Link>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
-      </div>
-    </nav>
-  );
+        </nav>
+    );
 }
 
 export default Navigation;
