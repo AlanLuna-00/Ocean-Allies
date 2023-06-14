e2c71d299797655d7d3f99ee04a6a2a0ecea7f90
Alan Luna <85804890+AlanLuna-00@users.noreply.github.com>
2023-06-14 20:00:47 -0300

    changes api next

--- client/src/app/api/auth/[...nextauth].js
+++ client/src/app/api/auth/[...nextauth].js
@@ -1,2 +1,12 @@
 import NextAuth from "next-auth";
-import Providers from "next-auth/providers";
+import GoogleProvider from "next-auth/providers";
+
+export default NextAuth({
+  // Configure one or more authentication providers
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+});
