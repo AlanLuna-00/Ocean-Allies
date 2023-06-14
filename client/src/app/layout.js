1b1d2160f97cef4d3a5679263fa47d74da773f29
Juan Mauricio Muñoz <maurikamon@hotmail.com>
2023-06-14 10:23:00 -0300

    footer update

--- client/src/app/layout.js
+++ client/src/app/layout.js
@@ -1,3 +1,4 @@
+"use client"
 import "./globals.css";
 import { Inter } from "next/font/google";
 import Navigation from "../components/Navigation";
--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,8 +1,25 @@
+"use client"
+import React, { useState, useEffect } from "react";
 import Link from "next/link";
 
 const Footer = () => {
+    const [showFooter, setShowFooter] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const isAtBottom = window.innerHeight + window.scrollY >= document.body.offsetHeight;
+            setShowFooter(isAtBottom);
+        };
+
+        window.addEventListener("scroll", handleScroll);
+
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
+
     return (
-        <footer className="fixed bottom-0 left-0 w-full bg-gray-800 text-gray-300">
+        <footer className={`fixed bottom-0 left-0 w-full bg-gray-800 text-gray-300 ${showFooter ? "" : "hidden"}`}>
             <div className="container mx-auto py-4">
                 <nav className="flex justify-center">
                     <Link href="/home" className="text-gray-300 hover:bg-gray-500 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Home</Link>
