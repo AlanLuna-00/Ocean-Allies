b62a9c19e88e7b3172feeccfd08b17ce01df0acb
Emanuel Nicolas Alessi <emanuelalessi14@gmail.com>
2023-06-14 16:04:15 -0300

    products actualizados

--- server/routes/productRoute.js
+++ server/routes/productRoute.js
@@ -2,14 +2,16 @@ const { Router } = require('express');
 const {
     getAllProductsController,
     getProductByIdController,
+    deleteProductController,
+    createProductController,
 } = require('../controllers/products.controller');
 
 const productRoute = Router();
 
 productRoute.get('/', getAllProductsController);
 productRoute.get('/:id', getProductByIdController);
-// productRoute.post('/', createProduct);
-// productRoute.delete('/:id', deleteProduct);
+productRoute.post('/', createProductController);
+productRoute.delete('/:id', deleteProductController);
 // productRoute.put('/:id', updateProduct);
 
 module.exports = productRoute;
