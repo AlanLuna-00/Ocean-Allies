2dba56e3d973ead8a759d6451fae1bcb7fbccd9f
MotionSoft05 <kevinbbarrios05@gmail.com>
2023-06-14 17:34:15 -0300

    PageUpdateMid

--- client/src/app/shop/page.jsx
+++ client/src/app/shop/page.jsx
@@ -23,23 +23,362 @@ const Shop = () => {
   }, [dispatch]);
 
   return (
-    <div className="container mx-auto">
-      <h1 className="text-2xl font-bold mb-4">Shop</h1>
-      <div className="grid grid-cols-2 gap-4">
-        {merchData.map((item) => (
-          <div key={item.id} className="bg-gray-200 p-4 rounded">
-            <img
-              src={item.avatar}
-              alt={item.first_name}
-              className="w-full h-auto mb-2"
-            />
-            <p className="text-gray-800 font-semibold">
-              {item.first_name} {item.last_name}
-            </p>
-            <p className="text-gray-600">{item.email}</p>
+    <div>
+      <main className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <div className="flex items-baseline justify-between border-b border-gray-200 pb-6">
+          <h1 className="text-4xl font-bold tracking-tight text-gray-900">
+            Shop
+          </h1>
+
+          <div className="flex items-center">
+            <div className="relative inline-block text-left">
+              <div>
+                <button
+                  type="button"
+                  className="group inline-flex justify-center text-sm font-medium text-gray-700 hover:text-gray-900"
+                  id="menu-button"
+                  aria-expanded="false"
+                  aria-haspopup="true"
+                >
+                  Sort
+                  <svg
+                    className="-mr-1 ml-1 h-5 w-5 flex-shrink-0 text-gray-400 group-hover:text-gray-500"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </button>
+              </div>
+              <div
+                className="absolute right-0 z-10 mt-2 w-40 origin-top-right rounded-md bg-white shadow-2xl ring-1 ring-black ring-opacity-5 focus:outline-none"
+                role="menu"
+                aria-orientation="vertical"
+                aria-labelledby="menu-button"
+                tabIndex="-1"
+              >
+                <div className="py-1" role="none">
+                  <a
+                    href="#"
+                    className="font-medium text-gray-900 block px-4 py-2 text-sm"
+                    role="menuitem"
+                    tabIndex="-1"
+                    id="menu-item-0"
+                  >
+                    Most Popular
+                  </a>
+                  <a
+                    href="#"
+                    className="text-gray-500 block px-4 py-2 text-sm"
+                    role="menuitem"
+                    tabIndex="-1"
+                    id="menu-item-1"
+                  >
+                    Best Rating
+                  </a>
+                  <a
+                    href="#"
+                    className="text-gray-500 block px-4 py-2 text-sm"
+                    role="menuitem"
+                    tabIndex="-1"
+                    id="menu-item-2"
+                  >
+                    Newest
+                  </a>
+                  <a
+                    href="#"
+                    className="text-gray-500 block px-4 py-2 text-sm"
+                    role="menuitem"
+                    tabIndex="-1"
+                    id="menu-item-3"
+                  >
+                    Price: Low to High
+                  </a>
+                  <a
+                    href="#"
+                    className="text-gray-500 block px-4 py-2 text-sm"
+                    role="menuitem"
+                    tabIndex="-1"
+                    id="menu-item-4"
+                  >
+                    Price: High to Low
+                  </a>
+                </div>
+              </div>
+            </div>
+            <button
+              type="button"
+              className="-m-2 ml-4 p-2 text-gray-400 hover:text-gray-500 sm:ml-6 lg:hidden"
+            >
+              <span className="sr-only">Filters</span>
+              <svg
+                className="h-5 w-5"
+                aria-hidden="true"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M2.628 1.601C5.028 1.206 7.49 1 10 1s4.973.206 7.372.601a.75.75 0 01.628.74v2.288a2.25 2.25 0 01-.659 1.59l-4.682 4.683a2.25 2.25 0 00-.659 1.59v3.037c0 .684-.31 1.33-.844 1.757l-1.937 1.55A.75.75 0 018 18.25v-5.757a2.25 2.25 0 00-.659-1.591L2.659 6.22A2.25 2.25 0 012 4.629V2.34a.75.75 0 01.628-.74z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </button>
           </div>
-        ))}
-      </div>
+        </div>
+
+        <section aria-labelledby="products-heading" className="pb-24 pt-6">
+          <h2 id="products-heading" className="sr-only">
+            Products
+          </h2>
+
+          <div className="grid grid-cols-1 gap-x-8 gap-y-10 lg:grid-cols-4">
+            <form className="hidden lg:block">
+              <h3 className="sr-only">Categories</h3>
+              <ul
+                role="list"
+                className="space-y-4 border-b border-gray-200 pb-6 text-sm font-medium text-gray-900"
+              >
+                <li>
+                  <a href="#">Totes</a>
+                </li>
+              </ul>
+
+              <div className="border-b border-gray-200 py-6">
+                <h3 className="-my-3 flow-root">
+                  <button
+                    type="button"
+                    className="flex w-full items-center justify-between bg-white py-3 text-sm text-gray-400 hover:text-gray-500"
+                    aria-controls="filter-section-0"
+                    aria-expanded="false"
+                  >
+                    <span className="font-medium text-gray-900">Color</span>
+                    <span className="ml-6 flex items-center">
+                      <svg
+                        className="h-5 w-5"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                        aria-hidden="true"
+                      >
+                        <path d="M10.75 4.75a.75.75 0 00-1.5 0v4.5h-4.5a.75.75 0 000 1.5h4.5v4.5a.75.75 0 001.5 0v-4.5h4.5a.75.75 0 000-1.5h-4.5v-4.5z" />
+                      </svg>
+                      <svg
+                        className="h-5 w-5"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                        aria-hidden="true"
+                      >
+                        <path
+                          fill-rule="evenodd"
+                          d="M4 10a.75.75 0 01.75-.75h10.5a.75.75 0 010 1.5H4.75A.75.75 0 014 10z"
+                          clip-rule="evenodd"
+                        />
+                      </svg>
+                    </span>
+                  </button>
+                </h3>
+                <div className="pt-6" id="filter-section-0">
+                  <div className="space-y-4">
+                    <div className="flex items-center">
+                      {/* <input id="filter-color-0" name="color[]" value="white" type="checkbox" className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"> */}
+                      <label
+                        for="filter-color-0"
+                        className="ml-3 text-sm text-gray-600"
+                      >
+                        White
+                      </label>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="border-b border-gray-200 py-6">
+                <h3 className="-my-3 flow-root">
+                  <button
+                    type="button"
+                    className="flex w-full items-center justify-between bg-white py-3 text-sm text-gray-400 hover:text-gray-500"
+                    aria-controls="filter-section-1"
+                    aria-expanded="false"
+                  >
+                    <span className="font-medium text-gray-900">Category</span>
+                    <span className="ml-6 flex items-center">
+                      <svg
+                        className="h-5 w-5"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                        aria-hidden="true"
+                      >
+                        <path d="M10.75 4.75a.75.75 0 00-1.5 0v4.5h-4.5a.75.75 0 000 1.5h4.5v4.5a.75.75 0 001.5 0v-4.5h4.5a.75.75 0 000-1.5h-4.5v-4.5z" />
+                      </svg>
+                      <svg
+                        className="h-5 w-5"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                        aria-hidden="true"
+                      >
+                        <path
+                          fill-rule="evenodd"
+                          d="M4 10a.75.75 0 01.75-.75h10.5a.75.75 0 010 1.5H4.75A.75.75 0 014 10z"
+                          clip-rule="evenodd"
+                        />
+                      </svg>
+                    </span>
+                  </button>
+                </h3>
+                <div className="pt-6" id="filter-section-1">
+                  <div className="space-y-4">
+                    <div className="flex items-center">
+                      {/* <input id="filter-category-0" name="category[]" value="new-arrivals" type="checkbox" className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"> */}
+                      <label
+                        for="filter-category-0"
+                        className="ml-3 text-sm text-gray-600"
+                      >
+                        New Arrivals
+                      </label>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="border-b border-gray-200 py-6">
+                <h3 className="-my-3 flow-root">
+                  <button
+                    type="button"
+                    className="flex w-full items-center justify-between bg-white py-3 text-sm text-gray-400 hover:text-gray-500"
+                    aria-controls="filter-section-2"
+                    aria-expanded="false"
+                  >
+                    <span className="font-medium text-gray-900">Size</span>
+                    <span className="ml-6 flex items-center">
+                      <svg
+                        className="h-5 w-5"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                        aria-hidden="true"
+                      >
+                        <path d="M10.75 4.75a.75.75 0 00-1.5 0v4.5h-4.5a.75.75 0 000 1.5h4.5v4.5a.75.75 0 001.5 0v-4.5h4.5a.75.75 0 000-1.5h-4.5v-4.5z" />
+                      </svg>
+                      <svg
+                        className="h-5 w-5"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                        aria-hidden="true"
+                      >
+                        <path
+                          fill-rule="evenodd"
+                          d="M4 10a.75.75 0 01.75-.75h10.5a.75.75 0 010 1.5H4.75A.75.75 0 014 10z"
+                          clip-rule="evenodd"
+                        />
+                      </svg>
+                    </span>
+                  </button>
+                </h3>
+                <div className="pt-6" id="filter-section-2">
+                  <div className="space-y-4">
+                    <div className="flex items-center">
+                      {/* <input id="filter-size-0" name="size[]" value="2l" type="checkbox" className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"> */}
+                      <label
+                        for="filter-size-0"
+                        className="ml-3 text-sm text-gray-600"
+                      >
+                        2L
+                      </label>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </form>
+
+            <div class="lg:col-span-3">
+              <div className="container mx-auto">
+                <div className="grid grid-cols-3 gap-4">
+                  {merchData.map((item) => (
+                    <div key={item.id} className="bg-gray-200 p-4 rounded">
+                      <img
+                        src={item.avatar}
+                        alt={item.first_name}
+                        className="w-full h-auto mb-2"
+                      />
+                      <p className="text-gray-800 font-semibold">
+                        {item.first_name} {item.last_name}
+                      </p>
+                      <p className="text-gray-600">{item.email}</p>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <nav
+          className="flex justify-center mb-8"
+          aria-label="Page navigation example"
+        >
+          <ul className="inline-flex -space-x-px">
+            <li>
+              <a
+                href="#"
+                className="px-3 py-2 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+              >
+                Previous
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+              >
+                1
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+              >
+                2
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                aria-current="page"
+                className="px-3 py-2 text-blue-600 border border-gray-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white"
+              >
+                3
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+              >
+                4
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+              >
+                5
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+              >
+                Next
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </main>
     </div>
   );
 };
