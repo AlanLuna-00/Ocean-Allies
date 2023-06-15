d35ddcb50123c98d04b773c4b2e590befa9b242d
pablopissoni <pablopissoni@gmail.com>
2023-06-15 20:11:26 -0300

    estetica about y contact

--- client/src/app/about/page.jsx
+++ client/src/app/about/page.jsx
@@ -16,23 +16,23 @@ export default function About() {
   const [isHovered, setIsHovered] = useState(false); //! Pendiente para usar en imagenes hover
 
   return (
-    <div className="bg-gray-100 py-12 px-6">
-      <h1 className="mb-2 mt-0 text-5xl font-medium leading-tight text-primary text-center">About</h1>
+    <div className="bg-gray-100 py-12 px-6 bg-cover" style={{ backgroundImage: "url(/img/marProfundo.jpg)" }}>
+      <h1 className="mb-2 mt-0 text-5xl font-bold leading-tight  text-center">About</h1>
 
       <div className="max-w-4xl mx-auto ">
-        <p className="text-2xl font-bold mb-6 text-center text-black grid  gap-8 block rounded-lg bg-gray-200 p-6 shadow-xl">
+        <p className="bg-opacity-60 shadow-xl text-2xl font-medium mb-6 text-center text-black grid  gap-8 block rounded-lg bg-white p-6">
           Ocean Allies es una plataforma dedicada a la conservación y protección de los animales marinos. 
           Nuestra misión es salvaguardar la vida en los océanos y preservar los hábitats marinos para las 
           generaciones futuras. Estamos comprometidos en crear conciencia sobre los desafíos que enfrentan 
           los animales marinos y promover acciones para mitigar las amenazas que enfrentan en su entorno.
         </p>
-        <h2 className="text-2xl font-bold mb-6 text-center text-black">Equipo de desarrolladores</h2>
-
-        <div className="grid grid-cols-4 gap-8 block rounded-lg bg-gray-800 p-6 shadow-xl">
+        <h2 className="bg-white rounded-lg bg-opacity-60 text-2xl font-bold mb-6 text-center text-black">Equipo de desarrolladores</h2>
+        
+        <div className="grid grid-cols-4 gap-8 block rounded-lg bg-gray-500 p-6 bg-opacity-60 shadow-xl">
           {developers.map((developer) => (
-            <div key={developer.name} className="flex flex-col items-center">
+            <div key={developer.name} className="flex flex-col items-center pb-2">
 
-              <div className="relative w-40 h-40 rounded-3xl overflow-hidden">
+              <div className="relative w-40 h-40 rounded-t-2xl overflow-hidden">
                 <Image
                   src={developer.image}
                   alt={developer.name}
@@ -47,10 +47,10 @@ export default function About() {
                 />
               </div>
 
-              <h5 className="text-lg font-semibold mt-4 text-gray-200">{developer.name}</h5>
-              <p className="text-neutral-500 dark:text-neutral-400">{developer.text}</p>
+              <div className='bg-neutral-200 relative w-40 pb-2 rounded-b-2xl'>
+              <h5 className="text-xl font-semibold mt-4 text-gray text-center">{developer.name}</h5>
+              <p className="text-neutral-500 dark:text-neutral-600 text-center">{developer.text}</p>
 
-              <div>
                 <ul className="mx-auto flex list-inside justify-center">
                 {/*<!-- GitHub -->*/}
                   <a href={developer.gitHub} className="px-2">
@@ -60,6 +60,7 @@ export default function About() {
                       d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
                   </svg>
                 </a>
+
                 {/*<!-- LinkedIn -->*/}
                 <a href={developer.linkedIn} className="px-2">
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="h-6 w-6 text-primary dark:text-primary-400 transition-all duration-300  hover:scale-125" >
