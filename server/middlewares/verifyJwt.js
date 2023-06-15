bb8b00fe821184e31934904ecabd89f49caf6929
Alan Luna <85804890+AlanLuna-00@users.noreply.github.com>
2023-06-15 13:07:16 -0300

    added jwt in review?

---
+++ server/middlewares/verifyJwt.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+
+function verifyJWT(req, res, next) {
+    const token = req.headers.authorization;
+
+    if (!token) {
+        return res.status(401).json({ message: 'Acceso no autorizado.' });
+    }
+
+    jwt.verify(token, process.env.SECRETORPRIVATEKEY, (err, decoded) => {
+        if (err) {
+            return res.status(401).json({ message: 'Token inválido.' });
+        }
+
+        // Agregar la información del usuario decodificada a la solicitud
+        req.user = decoded;
+        next();
+    });
+}
+
+module.exports = verifyJWT;
