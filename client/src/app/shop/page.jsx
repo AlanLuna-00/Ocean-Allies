d13a45022d8b4ea3376d8cc7ca61c00ca7ce4ea8
MotionSoft05 <kevinbbarrios05@gmail.com>
2023-06-16 11:31:59 -0300

    Shop mobile see-through filters solved

--- client/src/app/shop/page.jsx
+++ client/src/app/shop/page.jsx
@@ -115,7 +115,7 @@ const Shop = () => {
                 leaveFrom="translate-x-0"
                 leaveTo="translate-x-full"
               >
-                <Dialog.Panel className="relative ml-auto flex h-full w-full max-w-xs flex-col overflow-y-auto  py-4 pb-12 shadow-xl">
+                <Dialog.Panel className="relative ml-auto flex h-full w-full max-w-xs flex-col overflow-y-auto  py-4 pb-12 shadow-xl bg-white">
                   <div className="flex items-center justify-between px-4">
                     <h2 className="text-lg font-medium text-gray-900">
                       Filters
