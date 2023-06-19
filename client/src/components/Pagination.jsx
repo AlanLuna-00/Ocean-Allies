5231cbb3bccf5a78d800c9c8dedf6a34baad4aa0
Alan Luna <85804890+AlanLuna-00@users.noreply.github.com>
2023-06-19 17:55:53 -0300

    pagination working

--- client/src/components/Pagination.jsx
+++ client/src/components/Pagination.jsx
@@ -1,24 +1,32 @@
-import { useSelector } from "react-redux";
+import React from "react";
 
-const Pagination = () => {
-  const merchListData = useSelector((state) => state.merch.list);
-  // const totalPages = merchListData.info.totalPages;
-
-  const handleNextPage = () => {
-    console.log("next page");
+const Pagination = ({
+  currentPage,
+  totalPages,
+  nextPage,
+  previousPage,
+  onPageChange,
+}) => {
+  const handlePreviousPage = () => {
+    onPageChange(previousPage);
   };
 
-  const handlePreviousPage = () => {
-    console.log("previous page");
+  const handleNextPage = () => {
+    onPageChange(nextPage);
   };
 
   return (
     <nav aria-label="Page navigation example">
       <ul className="inline-flex items-center -space-x-px sm:flex sm:justify-center mb-8">
         <li>
-          <a
-            href="#"
-            className="block px-3 py-2 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+          <button
+            className={`block px-3 py-2 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg ${
+              previousPage
+                ? "hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                : "cursor-not-allowed dark:bg-gray-600 dark:border-gray-600 dark:text-gray-400"
+            }`}
+            onClick={handlePreviousPage}
+            disabled={!previousPage}
           >
             <span className="sr-only">Previous</span>
             <svg
@@ -34,46 +42,32 @@ const Pagination = () => {
                 clipRule="evenodd"
               ></path>
             </svg>
-          </a>
+          </button>
         </li>
+        {/* Renderizar los números de página */}
+        {Array.from({ length: totalPages }, (_, index) => (
+          <li key={index}>
+            <button
+              className={`px-3 py-2 leading-tight ${
+                currentPage === index + 1
+                  ? "text-blue-600 bg-blue-50 border border-gray-300 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white"
+                  : "text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+              }`}
+              onClick={() => onPageChange(index + 1)}
+            >
+              {index + 1}
+            </button>
+          </li>
+        ))}
         <li>
-          <a
-            href="#"
-            className="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-          >
-            1
-          </a>
-        </li>
-        <li>
-          <a
-            href="#"
-            className="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-          >
-            2
-          </a>
-        </li>
-        <li>
-          <a
-            href="#"
-            aria-current="page"
-            className="z-10 px-3 py-2 leading-tight text-blue-600 border border-blue-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white"
-          >
-            3
-          </a>
-        </li>
-        <li>
-          <a
-            href="#"
-            className="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-          >
-            4
-          </a>
-        </li>
-
-        <li>
-          <a
-            href="#"
-            className="block px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+          <button
+            className={`block px-3 py-2 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg ${
+              nextPage
+                ? "hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                : "cursor-not-allowed dark:bg-gray-600 dark:border-gray-600 dark:text-gray-400"
+            }`}
+            onClick={handleNextPage}
+            disabled={!nextPage}
           >
             <span className="sr-only">Next</span>
             <svg
@@ -85,11 +79,11 @@ const Pagination = () => {
             >
               <path
                 fillRule="evenodd"
-                d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 111.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
                 clipRule="evenodd"
               ></path>
             </svg>
-          </a>
+          </button>
         </li>
       </ul>
     </nav>
