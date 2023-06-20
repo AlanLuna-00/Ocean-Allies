7d5090bbdd036af5050254634b36d836a47a6053
pablopissoni <pablopissoni@gmail.com>
2023-06-20 18:54:34 -0300

    se agrego un alert y validacion en Contact

--- client/src/components/Message.jsx
+++ client/src/components/Message.jsx
@@ -1,12 +1,64 @@
 'use client'
-import React, { useRef, useState } from "react";
-import { validation } from "./validation";
+import React, { useEffect, useRef, useState } from "react";
+// import { validation } from "./validation";
 import emailjs from "emailjs-com";
+import Swal from 'sweetalert2';
+
 
 export default function Message() {
 
   const form = useRef();
   
+  
+  //* ------------------- SWEETALERT ------------------------
+  const showSuccess = () => {
+    Swal.fire({
+      position: 'top-end',
+      icon: 'success',
+      title: 'Your work has been saved',
+      showConfirmButton: false,
+      timer: 1500
+    });
+  }
+
+  const showError = () => {
+    Swal.fire({
+      position: 'top-end',
+      icon: 'error',
+      title: 'Message fail',
+      showConfirmButton: false,
+      timer: 1500
+    })
+  }
+  //* ------------------- SWEETALERT ------------------------
+  
+  //*-------------------- EMAILJS ------------------------
+  const sendEmail = (event) => {
+    event.preventDefault();
+    
+    const YOUR_SERVICE_ID = 'service_3khjvbt';
+    const YOUR_TEMPLATE_ID = 'template_0fpnoce';
+    const YOUR_PUBLIC_KEY = 'Dvtl9YLNRNuOmlvn4';
+    
+    emailjs.sendForm(YOUR_SERVICE_ID, YOUR_TEMPLATE_ID, form.current, YOUR_PUBLIC_KEY )
+    .then((result) => {
+      console.log(result.text);
+      setIsSent(true);
+      showSuccess(); // vetana de alerta exitosa
+    })
+    .catch((error) => {
+        console.log(error.text);
+        setIsSent(false);
+        showError();  // ventana de alerta de error
+      });    
+      
+      setUserData({
+        user_name: '',
+        user_email: "",
+        message: "",
+      });
+    };
+  //* ------------------- EMAILJS ---------------------------
   //* ------------------- VALIDACION ------------------------
   const [isSent, setIsSent] = useState(false);
 
@@ -16,43 +68,27 @@ export default function Message() {
     user_email: "",
     message: "",
   });
-  const [errors, setErrors] = useState({
-    //* HOOK Acumulador de errores
-    user_name: '',
-    user_email: "",
-    message: "",
-  });
+
+  const [error, setError] = useState({});
+
+  function validation (userData) {
+    let errors = {};
+    if (!userData.user_name || !/^[A-Za-z\s]{2,30}$/.test(userData.user_name )) errors.user_name = "Se requiere un nombre valido";
+
+    if (!userData.user_email || !/^[\w.%+-]+@[\w.-]+\.[a-zA-Z]{2,}$/.test(userData.user_email )) errors.user_email = "Se requiere un mail valido";
+
+    if ( !userData.message || userData.message.length > 400) errors.message = "Ingrese el mensaje menor a 400 caracteres"; 
+
+    setError(errors);
+  }
+  useEffect(() => {
+    validation(userData);
+  },[userData]);
   //* ------------------- VALIDACION ------------------------
- 
-
-  const sendEmail = (event) => {
-    event.preventDefault();
-
-    const YOUR_SERVICE_ID = 'service_3khjvbt';
-    const YOUR_TEMPLATE_ID = 'template_0fpnoce';
-    const YOUR_PUBLIC_KEY = 'Dvtl9YLNRNuOmlvn4';
-
-    emailjs.sendForm(YOUR_SERVICE_ID, YOUR_TEMPLATE_ID, form.current, YOUR_PUBLIC_KEY )
-      .then((result) => {
-        console.log(result.text);
-        setIsSent(true);
-      })
-      .catch((error) => {
-        console.log(error.text);
-        setIsSent(false);
-      });    
-
-    setUserData({
-      user_name: '',
-      user_email: "",
-      message: "",
-    });
-  };
-
+  
   const handleChange = (event) => {
     const { name, value } = event.target;
     setUserData({ ...userData, [name]: value });
-    validation({ ...userData, [name]: value }, errors, setErrors);
   };
 
 
@@ -72,7 +108,7 @@ export default function Message() {
             type="text"
             placeholder="Name"
           />
-          {errors.user_name && <p className="text-red-500">{errors.user_name}</p>}
+          {error.user_name && <p className="text-red-500">{error.user_name}</p>}
 
         </div>
         <div className="mb-6">
@@ -88,7 +124,7 @@ export default function Message() {
             type="email"
             placeholder="Email address"
           />
-          {errors.user_email && <p className="text-red-500">{errors.user_email}</p>}
+          {error.user_email && <p className="text-red-500">{error.user_email}</p>}
 
         </div>
         <div className="mb-6">
@@ -104,17 +140,21 @@ export default function Message() {
             rows="3"
             placeholder="Your message"
           ></textarea>
-          {errors.message && <p className="text-red-500">{errors.message}</p>}
+          {error.message && <p className="text-red-500">{error.message}</p>}
         </div>
 
         <button
-        type="submit"
-        value="Send"
-          className="bg-gray-800 w-full px-4 py-2 text-sm font-medium text-gray-100 bg-primary rounded-md shadow hover:bg-primary-600 focus:outline-none focus:ring-2 focus:ring-primary"
+          type="submit"
+          value="Send"
+          disabled={Object.keys(error).length > 0}
+          className={`w-full px-4 py-2 text-sm font-medium text-gray-100 rounded-md shadow focus:outline-none focus:ring-2 focus:ring-primary ${
+            Object.keys(error).length > 0 ? 'bg-gray-400 cursor-not-allowed' : 'bg-gray-800 hover:bg-gray-700'
+          }`}
         >
           Send
         </button>
-        {isSent && <p className="text-green-500">Message sent successfully!</p>}
+
+  {/* isSent && <p className="text-green-500">Message sent successfully!</p> */}
       </form>
     </div>
   );
