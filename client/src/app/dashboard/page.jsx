20919dd3324e676e09701eb8fb1326f6418992f4
Alan Luna <85804890+AlanLuna-00@users.noreply.github.com>
2023-06-22 12:28:09 -0300

    added axios in dashboard

--- client/src/app/dashboard/page.jsx
+++ client/src/app/dashboard/page.jsx
@@ -4,8 +4,28 @@ import Statistics from "../../components/dashboard/Statistics";
 import Slidebar from "../../components/dashboard/Slidebar";
 import Incomes from "../../components/dashboard/Incomes";
 import Users from "../../components/dashboard/Users";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const Dashboard = () => {
+  const [users, setUsers] = useState([]);
+
+  const fetchUsers = async () => {
+    const token = localStorage.getItem("token");
+    const replaceToken = token.replace(/['"]+/g, "");
+    console.log(replaceToken);
+    const res = await axios("http://localhost:8080/api/users", {
+      headers: {
+        Authorization: replaceToken,
+      },
+    });
+    console.log(res.data);
+    setUsers(res.data);
+  };
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
   return (
     <div className="antialiased bg-black w-full min-h-screen text-slate-300 relative py-4">
       <div className="grid grid-cols-12 mx-auto gap-2 sm:gap-4 md:gap-6 lg:gap-10 xl:gap-14 max-w-7xl my-10 px-2">
