6055d49cbf564f90d56dde824f42acb73b15d9d8
Alan Luna <85804890+AlanLuna-00@users.noreply.github.com>
2023-06-21 22:58:20 -0300

    added first fetch

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,6 +1,23 @@
 import Header from "@/components/Header";
+import { useDispatch } from "react-redux";
+import { setMerchList } from "@/store/merchSlice";
+import axios from "axios";
 
 function Page() {
+  const dispatch = useDispatch();
+  const fetchMerchList = async () => {
+    try {
+      const response = await axios.get("http://localhost:8080/api/products");
+      dispatch(setMerchList(products));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchMerchList();
+  }, []);
+
   return (
     <div className="relative inset-0 w-full min-h-screen md:fixed sm:fixed min-[120px]:fixed">
       <Header />
