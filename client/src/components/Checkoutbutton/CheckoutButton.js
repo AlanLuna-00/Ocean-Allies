32e7506eb4424e30c7c0505dd7f5b35ce2c622d7
Emanuel Nicolas Alessi <emanuelalessi14@gmail.com>
2023-06-23 17:00:41 -0300

    mp act

--- client/src/components/Checkoutbutton/CheckoutButton.js
+++ client/src/components/Checkoutbutton/CheckoutButton.js
@@ -1,23 +1,30 @@
-import axios from 'axios';
-import { useState } from 'react';
+import axios from "axios";
 
+const CheckoutButton = ({ price }) => {
+  const handleClick = async () => {
+    try {
+      const response = await axios.post(
+        "http://localhost:8080/api/payment/create-order",
+        {
+          price: price,
+        }
+      );
+      const data = response.data;
+      window.location.href = data.init_point;
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-const CheckoutButton = () => {
-    const handleClick = async () => {
-        const response = await axios.post("http://localhost:8080/api/payment/create-order")
-        const data = response.data;
-        window.location.href = data.init_point;
-    };
-
-    return (
-        <button
-            id="checkout"
-            className="flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm"
-            onClick={handleClick}
-        >
-            PAY
-        </button>
-    );
+  return (
+    <button
+      id="checkout"
+      className="flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm"
+      onClick={handleClick}
+    >
+      PAY
+    </button>
+  );
 };
 
 export default CheckoutButton;
