9cb6792e16d748c77ef7e71989fa28140399a0a2
Juan Mauricio Muñoz <maurikamon@hotmail.com>
2023-06-23 21:31:34 -0300

    home actualizado con transciciones

--- client/src/app/home/page.jsx
+++ client/src/app/home/page.jsx
@@ -1,12 +1,18 @@
+"use client"
 import FotoHome from "@/components/FotoHome";
 import Testimonials from "@/components/Testimonials";
+import { motion } from "framer-motion";
 
 export default function Home() {
   return (
     <div className="container mx-auto p-10 md:max-w-full">
       {/*  */}
       <div className="">
-        <div class="text-center max-w-xl mx-auto">
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ delay: 0.2, duration: 0.5 }}
+          class="text-center max-w-xl mx-auto">
           <h1 class="text-6xl md:text-7xl font-bold mb-5 text-gray-600">
             The ocean is also <br />
             our land.
@@ -18,11 +24,15 @@ export default function Home() {
             <span class="inline-block w-3 h-1 rounded-full bg-blue-600 ml-1"></span>
             <span class="inline-block w-1 h-1 rounded-full bg-blue-600 ml-1"></span>
           </div>
-        </div>
+        </motion.div>
 
         <div className="grid grid-cols-1 lg:grid-cols-3 md:grid-cols-1 gap-10 md:max-w-full">
           {/* px-4 pt-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 */}
-          <div className="col-span-1 relative ">
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 0.4, duration: 1 }}
+            className="col-span-1 relative ">
             {/* Contenido de la primera columna */}
             <h1 className="text-4xl md:text-2xl font-bold mb-10 text-gray-100 mt-0 text-center uppercase absolute top-20 z-10 sm:text-lg">
               NGO dedicated to the protection and conservation of the marine
@@ -32,8 +42,12 @@ export default function Home() {
               className="h-64 bg-cover bg-center flex flex-col items-center justify-center hover:blur-sm transition-all duration-500 ease-in-out rounded-md shadow-2xl "
               style={{ backgroundImage: "url(/img/delfin.jpg) " }}
             ></div>
-          </div>
-          <div className="col-span-1 relative">
+          </motion.div>
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 0.8, duration: 1 }}
+            className="col-span-1 relative">
             {/* Contenido de la segunda columna */}
             <h1 className="text-4xl md:text-2xl font-bold mb-10 text-gray-100 mt-0 text-center uppercase absolute top-20 z-10">
               We specialize in research and monitoring of marine species.
@@ -42,8 +56,12 @@ export default function Home() {
               className="h-64 bg-cover bg-center flex flex-col items-center justify-center hover:blur-sm transition-all duration-500 ease-in-out  rounded-md shadow-2xl"
               style={{ backgroundImage: "url(/img/foca.jpg)" }}
             ></div>
-          </div>
-          <div className="col-span-1 relative">
+          </motion.div>
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 1, duration: 1 }}
+            className="col-span-1 relative">
             {/* Contenido de la tercera columna */}
             <h1 className="text-4xl md:text-2xl font-bold mb-10 text-gray-100 mt-0 text-center uppercase absolute top-20 z-10">
               Awareness campaigns and educational programs.
@@ -52,13 +70,21 @@ export default function Home() {
               className="h-64 bg-cover bg-center flex flex-col items-center justify-center hover:blur-sm transition-all duration-500 ease-in-out  rounded-md shadow-2xl"
               style={{ backgroundImage: "url(/img/corales.jpg)" }}
             ></div>
-          </div>
+          </motion.div>
         </div>
-        <div className="md:max-w-full py-12 rounded-md bg-cover h-64 mt-10">
+        <motion.div
+          initial={{ opacity: 0, x: -100 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ delay: 1.2, duration: 1 }}
+          className="md:max-w-full py-12 rounded-md bg-cover h-64 mt-10">
           <FotoHome />
-        </div>
+        </motion.div>
       </div>
-      <div class="text-center max-w-xl mx-auto p-20 mt-10  ">
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 4, duration: 1 }}
+        class="text-center max-w-xl mx-auto p-20 mt-10  ">
         <div class="text-center mb-10">
           <span class="inline-block w-1 h-1 rounded-full bg-blue-600 ml-1"></span>
           <span class="inline-block w-3 h-1 rounded-full bg-blue-600 ml-1"></span>
@@ -80,9 +106,13 @@ export default function Home() {
           <span class="inline-block w-3 h-1 rounded-full bg-blue-600 ml-1"></span>
           <span class="inline-block w-1 h-1 rounded-full bg-blue-600 ml-1"></span>
         </div>
-      </div>
+      </motion.div>
       {/*  */}
-      <div className=" grid grid-cols-1 lg:grid-cols-2 md:grid-cols-1 gap-20 md:max-w-full ">
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 5, duration: 1 }}
+        className=" grid grid-cols-1 lg:grid-cols-2 md:grid-cols-1 gap-20 md:max-w-full ">
         <div className="col-span-1">
           {/* Contenido de la tercera columna */}
           <div
@@ -105,7 +135,7 @@ export default function Home() {
             </h1>
           </div>
         </div>
-      </div>
+      </motion.div>
       <Testimonials />
     </div>
   );
