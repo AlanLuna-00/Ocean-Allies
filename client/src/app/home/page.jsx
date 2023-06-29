659b9ef6137a1cd5804877223e225cd5c691bfec
MotionSoft05 <kevinbbarrios05@gmail.com>
2023-06-29 13:15:12 -0300

    Revert "Home Responsive"
    This reverts commit a40de2b6097aabda77dab3d5d1b10ddc3e5648dc.

--- client/src/app/home/page.jsx
+++ client/src/app/home/page.jsx
@@ -1,8 +1,7 @@
-"use client";
+"use client"
 import FotoHome from "@/components/FotoHome";
 import Testimonials from "@/components/Testimonials";
 import { motion } from "framer-motion";
-import Link from "next/link";
 
 export default function Home() {
   return (
@@ -13,8 +12,7 @@ export default function Home() {
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           transition={{ delay: 0.2, duration: 0.5 }}
-          className="text-center max-w-xl mx-auto"
-        >
+          className="text-center max-w-xl mx-auto">
           <h1 className="text-6xl md:text-7xl font-bold mb-5 text-gray-600">
             The ocean is also <br />
             our land.
@@ -34,10 +32,9 @@ export default function Home() {
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             transition={{ delay: 0.4, duration: 1 }}
-            className="col-span-1 relative "
-          >
+            className="col-span-1 relative ">
             {/* Contenido de la primera columna */}
-            <h1 className="text-2xl sm:text-4xl font-bold mb-10 text-gray-100 mt-0 text-center uppercase absolute top-20 z-10 ">
+            <h1 className="text-4xl md:text-2xl font-bold mb-10 text-gray-100 mt-0 text-center uppercase absolute top-20 z-10 sm:text-lg">
               NGO dedicated to the protection and conservation of the marine
               world.
             </h1>
@@ -50,10 +47,9 @@ export default function Home() {
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             transition={{ delay: 0.8, duration: 1 }}
-            className="col-span-1 relative"
-          >
+            className="col-span-1 relative">
             {/* Contenido de la segunda columna */}
-            <h1 className="text-2xl sm:text-4xl font-bold mb-10 text-gray-100 mt-0 text-center uppercase absolute top-20 z-10">
+            <h1 className="text-4xl md:text-2xl font-bold mb-10 text-gray-100 mt-0 text-center uppercase absolute top-20 z-10">
               We specialize in research and monitoring of marine species.
             </h1>
             <div
@@ -65,10 +61,9 @@ export default function Home() {
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             transition={{ delay: 1, duration: 1 }}
-            className="col-span-1 relative"
-          >
+            className="col-span-1 relative">
             {/* Contenido de la tercera columna */}
-            <h1 className="text-2xl md:text-4xl font-bold mb-10 text-gray-100 mt-0 text-center uppercase absolute top-20 z-10">
+            <h1 className="text-4xl md:text-2xl font-bold mb-10 text-gray-100 mt-0 text-center uppercase absolute top-20 z-10">
               Awareness campaigns and educational programs.
             </h1>
             <div
@@ -81,8 +76,7 @@ export default function Home() {
           initial={{ opacity: 0, x: -100 }}
           animate={{ opacity: 1, x: 0 }}
           transition={{ delay: 1.2, duration: 1 }}
-          className="md:max-w-full py-12 rounded-md bg-cover h-64 mt-10"
-        >
+          className="md:max-w-full py-12 rounded-md bg-cover h-64 mt-10">
           <FotoHome />
         </motion.div>
       </div>
@@ -90,8 +84,7 @@ export default function Home() {
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         transition={{ delay: 4, duration: 1 }}
-        className="text-center max-w-xl mx-auto p-20 mt-10  "
-      >
+        className="text-center max-w-xl mx-auto p-20 mt-10  ">
         <div className="text-center mb-10">
           <span className="inline-block w-1 h-1 rounded-full bg-blue-600 ml-1"></span>
           <span className="inline-block w-3 h-1 rounded-full bg-blue-600 ml-1"></span>
@@ -99,10 +92,10 @@ export default function Home() {
           <span className="inline-block w-3 h-1 rounded-full bg-blue-600 ml-1"></span>
           <span className="inline-block w-1 h-1 rounded-full bg-blue-600 ml-1"></span>
         </div>
-        <h1 className="text-2xl md:text-6xl font-bold mb-5 text-gray-600">
+        <h1 className="text-6xl md:text-6xl font-bold mb-5 text-gray-600 ">
           IT’S TIME TO TAKE ACTION!
         </h1>
-        <h3 className="text-2xl md:text-6xl font-bold mb-5 text-gray-600">
+        <h3 className="text-6xl md:text-3xl font-bold mb-5 text-gray-600">
           We are all part of the problem and the solution. Get involved and help
           us keep the ocean clean.
         </h3>
@@ -119,31 +112,28 @@ export default function Home() {
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         transition={{ delay: 5, duration: 1 }}
-        className="grid grid-cols-1 lg:grid-cols-2 md:grid-cols-1 gap-20 md:max-w-full"
-      >
+        className=" grid grid-cols-1 lg:grid-cols-2 md:grid-cols-1 gap-20 md:max-w-full ">
         <div className="col-span-1">
           {/* Contenido de la tercera columna */}
-          <Link
-            href="/shop"
+          <div
             className="h-64 bg-cover bg-center flex flex-col items-center justify-center"
             style={{ backgroundImage: "url(/img/shop.jpg)" }}
           >
             <h1 className="text-4xl md:text-3xl font-bold mb-10 text-gray-100 mt-0 text-center uppercase">
               SHOP
             </h1>
-          </Link>
+          </div>
         </div>
         <div className="col-span-1">
           {/* Contenido de la tercera columna */}
-          <Link
-            href="/#"
+          <div
             className="h-64 bg-cover bg-center flex flex-col items-center justify-center"
             style={{ backgroundImage: "url(/img/manos.jpg)" }}
           >
             <h1 className="text-4xl md:text-3xl font-bold mb-10 text-gray-100 mt-0 text-center uppercase">
               DONATIONS
             </h1>
-          </Link>
+          </div>
         </div>
       </motion.div>
       <Testimonials />
