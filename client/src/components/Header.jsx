7afd6e6761cdefb7c0db7972d1e5dcbfe1f18cf9
Alan Luna <85804890+AlanLuna-00@users.noreply.github.com>
2023-07-03 12:23:49 -0300

    deploy

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -48,7 +48,7 @@ const Header = () => {
                   href="/home"
                   className="inline-block rounded border-2 border-neutral-50 px-6 pb-[6px] pt-2 text-lg font-medium uppercase leading-normal text-neutral-50 transition duration-150 ease-in-out hover:border-neutral-100 hover:bg-neutral-500 hover:bg-opacity-10 hover:text-neutral-100 focus:border-neutral-100 focus:text-neutral-100 focus:outline-none focus:ring-0 active:border-neutral-200 active:text-neutral-200 dark:hover:bg-neutral-100 dark:hover:bg-opacity-10"
                 >
-                  Home
+                  Go to Home
                 </Link>
               </motion.div>
             </div>
