cda6412384f21143136c574103b08ee155fde602
Emanuel Nicolas Alessi <emanuelalessi14@gmail.com>
2023-07-03 23:56:10 -0300

    pull de imageprofile

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -8,16 +8,17 @@ const {
     updateUserPassword,
 } = require('../controllers/user.controller');
 const verifyJWT = require('../middlewares/verifyJwt');
+const upload = require('../middleware/multer');
 
 const userRoute = Router();
 
-// userRoute.use(verifyJWT);
+//userRoute.use(verifyJWT);
 
 userRoute.get('/', verifyJWT, getAllUsers);
 userRoute.get('/:id', verifyJWT, getUserById);
 userRoute.post('/', verifyJWT, createUser);
 userRoute.delete('/:id', verifyJWT, deleteUser);
-userRoute.put('/:id', verifyJWT, updateUser);
+userRoute.put('/:id', upload.single('image'), updateUser);
 userRoute.put('/password/:id', updateUserPassword);
 
 module.exports = userRoute;
