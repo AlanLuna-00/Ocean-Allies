571908fd81c7d868159c957c0b0d1c0e1d371b80
Alan Luna <85804890+AlanLuna-00@users.noreply.github.com>
2023-07-04 19:27:50 -0300

    Update db.js

--- server/db.js
+++ server/db.js
@@ -2,15 +2,29 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT } = process.env;
+const {
+    DB_USER,
+    DB_PASSWORD,
+    DB_HOST,
+    DB_NAME,
+    DB_PORT,
+    PRODUCTION,
+    DB_DEPLOY,
+} = process.env;
 
-const sequelize = new Sequelize(
-    `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
-    {
-        logging: false,
-        native: false,
-    }
-);
+const sequelize =
+    PRODUCTION === 'true'
+        ? new Sequelize(DB_DEPLOY, {
+              logging: false,
+              native: false,
+          })
+        : new Sequelize(
+              `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
+              {
+                  logging: false,
+                  native: false,
+              }
+          );
 
 const basename = path.basename(__filename);
 
