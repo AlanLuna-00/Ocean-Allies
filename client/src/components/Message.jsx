2ea6689fe1ae31df36a0ff8cfe5b8cdbab2f7e38
Alan Luna <85804890+AlanLuna-00@users.noreply.github.com>
2023-07-05 14:01:08 -0300

    fixed emailjs

--- client/src/components/Message.jsx
+++ client/src/components/Message.jsx
@@ -1,15 +1,12 @@
-'use client'
+"use client";
 import React, { useEffect, useRef, useState } from "react";
 // import { validation } from "./validation";
 import emailjs from "emailjs-com";
 import { showSuccessMessage, showErrorMessage } from "./SweetAlerts";
 
-
 export default function Message() {
-
   const [error, setError] = useState({});
   const form = useRef();
-
 
   //*-------------------- EMAILJS ------------------------
   const sendEmail = (event) => {
@@ -21,35 +18,40 @@ export default function Message() {
     // }
     // ! validacion test ---------------
     const errors = {
-      name: !userData.user_name,
-      email: !userData.user_email,
+      name: !userData.from_name,
+      email: !userData.to_email,
       message: !userData.message,
-    }
+    };
     setError(errors);
     const hasError = Object.values(errors).some(Boolean);
     if (hasError) {
       return;
     }
     // ! validacion test ---------------
-    const YOUR_SERVICE_ID = 'service_3khjvbt';
-    const YOUR_TEMPLATE_ID = 'template_0fpnoce';
-    const YOUR_PUBLIC_KEY = 'Dvtl9YLNRNuOmlvn4';
+    const YOUR_SERVICE_ID = "service_3khjvbt";
+    const YOUR_TEMPLATE_ID = "template_0fpnoce";
+    const YOUR_PUBLIC_KEY = "Dvtl9YLNRNuOmlvn4";
 
-    emailjs.sendForm(YOUR_SERVICE_ID, YOUR_TEMPLATE_ID, form.current, YOUR_PUBLIC_KEY)
+    emailjs
+      .sendForm(
+        YOUR_SERVICE_ID,
+        YOUR_TEMPLATE_ID,
+        form.current,
+        YOUR_PUBLIC_KEY
+      )
       .then((result) => {
-
         setIsSent(true);
         showSuccessMessage(); // vetana de alerta exitosa
       })
       .catch((error) => {
         console.log(error.text);
         setIsSent(false);
-        showErrorMessage();  // ventana de alerta de error
+        showErrorMessage(); // ventana de alerta de error
       });
 
     setUserData({
-      user_name: '',
-      user_email: "",
+      from_name: "",
+      to_email: "",
       message: "",
     });
   };
@@ -59,8 +61,8 @@ export default function Message() {
 
   const [userData, setUserData] = useState({
     //* HOOK Acumulador de datos
-    user_name: '',
-    user_email: "",
+    from_name: "",
+    to_email: "",
     message: "",
   });
 
@@ -71,7 +73,7 @@ export default function Message() {
     setUserData({ ...userData, [name]: value });
   };
 
-  console.log(error)
+  console.log(error);
   return (
     <div className="max-w-md mx-auto rounded-lg bg-slate-200 shadow-xl p-6">
       <form ref={form} onSubmit={sendEmail}>
@@ -80,39 +82,46 @@ export default function Message() {
             Name <span className="text-red-500">*</span>
           </label>
           <input
-            className={`${error.name ? 'border-red-500' : ''} w-full px-3 py-2 leading-tight text-gray-700 border 
+            className={`${
+              error.name ? "border-red-500" : ""
+            } w-full px-3 py-2 leading-tight text-gray-700 border 
             border-gray-400 rounded appearance-none focus:outline-none focus:border-primary focus:ring-primary`}
             id="name"
-            name="user_name"
-            value={userData.user_name}
+            name="from_name"
+            value={userData.from_name}
             onChange={handleChange}
             type="text"
             placeholder="Name"
           />
-
         </div>
         <div className="mb-6">
           <label className="block mb-1 font-bold text-gray-700" htmlFor="email">
             Email address <span className="text-red-500">*</span>
           </label>
           <input
-            className={`${error.email ? 'border-red-500' : ''} w-full px-3 py-2 leading-tight text-gray-700 border 
+            className={`${
+              error.email ? "border-red-500" : ""
+            } w-full px-3 py-2 leading-tight text-gray-700 border 
             border-gray-400 rounded appearance-none focus:outline-none focus:border-primary focus:ring-primary`}
             id="email"
-            name="user_email"
-            value={userData.user_email}
+            name="to_email"
+            value={userData.to_email}
             onChange={handleChange}
             type="email"
             placeholder="Email address"
           />
-
         </div>
         <div className="mb-6">
-          <label className="block mb-1 font-bold text-gray-700" htmlFor="message">
+          <label
+            className="block mb-1 font-bold text-gray-700"
+            htmlFor="message"
+          >
             Message <span className="text-red-500">*</span>
           </label>
           <textarea
-            className={`${error.message ? 'border-red-500' : ''} w-full px-3 py-2 leading-tight text-gray-700 border border-gray-400 
+            className={`${
+              error.message ? "border-red-500" : ""
+            } w-full px-3 py-2 leading-tight text-gray-700 border border-gray-400 
             rounded resize-none h-32 appearance-none focus:outline-none focus:border-primary focus:ring-primary`}
             id="message"
             name="message"
@@ -129,9 +138,9 @@ export default function Message() {
         <button
           type="submit"
           value="Send"
-          className='w-full px-4 py-2 text-sm font-medium text-gray-100 rounded-md 
+          className="w-full px-4 py-2 text-sm font-medium text-gray-100 rounded-md 
                     shadow focus:outline-none focus:ring-2 focus:ring-primary
-                  bg-gray-800 hover:bg-gray-700'
+                  bg-gray-800 hover:bg-gray-700"
         >
           Send
         </button>
